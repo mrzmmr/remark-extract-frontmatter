406e0ad444175a7ea67b08a829b39970cfa1e2be
mrzmmr <mrzmmr@gmail.com>
2018-09-20 21:59:49 +0000

    Add tests
    Add initial tests.
    [TODO] clean up and refactor tests

--- test.js
+++ test.js
@@ -1,69 +1,109 @@
-const {test} = require('tap')
-const frontmatter = require('remark-frontmatter')
-const stringify = require('remark-stringify')
-const parser = require('remark-parse')
-const unified = require('unified')
-const {parse: yaml} = require('yamljs')
+var unified = require('unified')
+var parser = require('remark-parse')
+var compiler = require('remark-stringify')
+var frontmatter = require('remark-frontmatter')
+var yaml = require('yaml').parse
+var toml = require('toml').parse
+var test = require('tap').test
 
-const extractFrontmatter = require('.')
+var extract = require('.')
 
-const markdown = `
----
-title: "Hello World!"
----
+var okYaml = '---\ntest: ok\n---\n'
+var okToml = '+++\ntest = "ok"\n+++\n'
+var badToml = '+++\ntest: ok\n+++\n'
 
-# Other Markdown
-`
+test('remark-extract-frontmatter', function (t) {
+  var processor = unified()
+    .use(parser)
+    .use(compiler)
 
-test('remark-extract-frontmatter', t => {
-  let file
-
-  t.test('Should store frontmatter in `vfile.data.frontmatter`', it => {
-    it.doesNotThrow(() => {
-      file = unified()
-        .use(parser)
-        .use(stringify)
+  t.doesNotThrow(function () {
+    t.test('Does not modify tokenizer.', function (it) {
+      var file = processor()
         .use(frontmatter)
-        .use(extractFrontmatter)
-        .processSync(markdown)
+        .use(extract)
+        .processSync(okYaml)
 
-      it.ok(Array.isArray(file.data.frontmatter))
-      it.ok(file.data.frontmatter.length === 1)
-      it.ok(file.data.frontmatter[0] === 'title: "Hello World!"')
-    }, 'No options')
+      it.ok(file)
+      it.ok(JSON.stringify(file.data) === '{}')
 
-    it.doesNotThrow(() => {
-      file = unified()
-        .use(parser)
-        .use(stringify)
+      file = processor()
+        .use(extract, toml)
+        .processSync(okYaml)
+
+      it.ok(file)
+      it.end()
+    })
+    t.test('Extracts frontmatter given parser', function (it) {
+      var file = processor()
         .use(frontmatter)
-        .use(extractFrontmatter, {type: 'yaml', parser: yaml})
-        .processSync(markdown)
+        .use(extract, yaml)
+        .processSync(okYaml)
 
-      it.ok(Array.isArray(file.data.frontmatter))
-      // It.ok(typeof file.data.frontmatter[0] === 'object')
-      it.same(file.data.frontmatter[0], {title: 'Hello World!'})
-    }, 'With parser option')
+      it.ok(file.data.test)
+      it.ok(file.data.test === 'ok')
+      it.end()
+    })
+    t.test('Works with options', function (it) {
+      var file = processor()
+        .use(frontmatter, [ 'toml' ])
+        .use(extract, { toml: toml })
+        .processSync(okToml)
 
-    it.doesNotThrow(() => {
-      const start = unified()
-        .use(parser)
-        .use(stringify)
-        .processSync(markdown)
-
-      start.data.frontmatter = ['oops']
-
-      file = unified()
-        .use(parser)
-        .use(stringify)
+      it.ok(file.data.test)
+      it.ok(file.data.test === 'ok')
+      it.end()
+    })
+    t.test('Set a name to store frontmatter under', function (it) {
+      var file = processor()
         .use(frontmatter)
-        .use(extractFrontmatter, {type: 'toml', parser: yaml})
-        .processSync(start)
+        .use(extract, { yaml: yaml, name: 'frontmatter' })
+        .processSync(okYaml)
 
-      it.ok(file.data.frontmatter.length === 1)
-    }, 'With wrong args')
+      it.ok(file.data)
+      it.ok(file.data.frontmatter)
+      it.ok(file.data.frontmatter.test === 'ok')
+      it.end()
+    })
 
+    t.test('Creates a VFile message when parser fails to parse frontmatter', function (it) {
+      var file = processor()
+        .use(frontmatter, [ 'toml' ])
+        .use(extract, { toml: toml })
+        .processSync(badToml)
+
+      it.ok(file.data)
+      it.ok(JSON.stringify(file.data) === '{}')
+      it.ok(file.messages[0])
+      it.ok(file.messages[0].message === 'Expected "=", [ \\t] or [A-Za-z0-9_\\-] but ":" found.')
+      it.end()
+    })
+  })
+
+  t.doesNotThrow(function () {
+    unified()
+      .use(parser)
+      .use(compiler)
+      .use(frontmatter)
+      .use(extract, { toml: toml })
+      .freeze()
+  }, 'should not throw if wrong parsing function is given')
+
+  t.doesNotThrow(function () {
+    unified()
+      .use(extract, yaml)
+      .freeze()
+  }, 'should not throw without parser')
+
+  t.test('Should throw with `options.throws` and bad frontmatter', function (it) {
+    it.throws(function () {
+      processor()
+        .use(frontmatter, [ 'toml' ])
+        .use(extract, { toml: toml, throws: true })
+        .processSync(badToml)
+    })
     it.end()
   })
+
   t.end()
 })
