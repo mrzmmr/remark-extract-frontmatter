82863389e25e8a17bfe6a6c372bd870f3c161c41
mrzmmr <mrzmmr@gmail.com>
2020-10-24 20:14:34 -0700

    Refactor Refactor to just be a transformer since there is no reason to touch the parser.

--- index.js
+++ index.js
@@ -1,184 +1,75 @@
-module.exports = extract
+module.exports = function extract(options = {}) {
+    return function (tree, file) {
+        let name
 
-var xtend = require('xtend')
-
-function extract (options) {
-  var blockTokenizers
-  var blockMethods
-  var tokenizer
-  var parsers
-  var methods
-  var method
-  var parse
-  var parser
-  var proto
-  var name
-  var i
-
-  options = settings(options)
-
-  if (!options || !options.parsers) {
-    return
-  }
-
-  parsers = options.parsers
-  parser = this.Parser
-
-  if (isParser(parser)) {
-    proto = parser.prototype
-    blockTokenizers = proto.blockTokenizers
-    blockMethods = proto.blockMethods
-    methods = locate(blockMethods)
-
-    if (!methods || methods.length < 1) {
-      return
-    }
-
-    i = -1
-
-    while (++i < methods.length){
-      /**
-       * Block method to identify tokenizer
-       */
-      method = methods[i]
-      /**
-       * Name of the parser for frontmatter, e.g. "yaml"
-       */
-      name = method.replace('FrontMatter', '')
-      /**
-       * Actual function to parse frontmatter, e.g. `yaml.parse`
-       */
-      parse = parsers[name]
-      /**
-       * The tokenizer to decorate
-       */
-      tokenizer = blockTokenizers[method]
-      /**
-       * Only decorate if a parser was given
-       */
-      if (parse) {
-        /**
-         * Replace Tokenizer with decorated tokenizer
-         */
-        blockTokenizers[method] = decorate(tokenizer, parse, name, options)
-      }
-    }
-  }
-}
-
-/**
- * Returns a decorator for blockTokenizer
- * tokenizer - The tokenizer to decorate, e.g. yamlFrontMatter
- * parse - A parse function to run on found frontmatter, e.g. `yamljs.parse`
- * name - The name of type of frontmatter to be parsed, e.g. "yaml"
- * options - Any options to use
- */
-function decorate (tokenizer, parse, name, options) {
-  return decorator
-
-  function decorator (eat, value, silent) {
-    var node = tokenizer(eat, value, silent)
-    var file = this.file
-    var frontmatter
-    var data
-
-
-    if (node && node.value) {
-      try {
-        frontmatter = parse(node.value)
-
-        if (options.name) {
-          file.data[options.name] = file.data[options.name] || {}
-          file.data[options.name] = xtend({}, file.data[options.name], frontmatter)
-        } else {
-          file.data = xtend({}, file.data, frontmatter)
+        if (typeof options === 'function') {
+            options.yaml = options
         }
-      } catch (err) {
-        error(err, file, name, options)
-      }
 
-      return node
+        if (typeof options !== 'function' && options.name) {
+            name = options.name
+        }
+
+        each(tree, file)
+
+        function each(node) {
+            if (extract(node)) {
+                return 1
+            }
+
+            return every(node)
+        }
+
+        function every({ children }) {
+            if (!children) {
+                return
+            }
+
+            for (child of children) {
+                if (each(child)) {
+                    return 1
+                }
+            }
+
+            return
+        }
+
+        function extract({ type, value }) {
+            const check = options[type]
+
+            if (!check || typeof check !== 'function') {
+                return
+            }
+
+            try {
+                const result = check(value)
+
+                if (!result) {
+                    return
+                }
+
+                if (name) {
+                    file.data[name] = file.data[name] || {}
+
+                    Object.assign(file.data[name], result)
+                } else {
+                    Object.assign(file.data, result)
+                }
+
+                return 1
+            } catch (error) {
+                return maybeThrow(error, type)
+            }
+        }
+
+        function maybeThrow({ message, column, line, name: type }, name) {
+            let method = options.throws
+                ? "fail"
+                : "message"
+
+            file[method](message, { line, column }, `parseFrontMatter:${name}:${type}`)
+
+            return
+        }
     }
-  }
-}
-
-/**
- * Handles errors such as creating a new `vfile.message` when parsing fails
- * err - The Error object produced
- * name - The name of type of frontmatter, e.g. 'yaml'
- * options - Any options
- */
-function error (err, file, name, options) {
-  var message = err.message
-  var column = err.column
-  var line = err.line
-  var type = err.name
-  var method = 'message'
-
-  if (options.throws) {
-    method = 'fail'
-  }
-
-  file[method](
-    message,
-    { line: line, column: column },
-    'parseFrontMatter:' + name + ':' + type
-  )
-}
-
-
-/**
- * Locate any frontMatter methods that have been attatched to the Parser
- * methods - A list of blockMethods, e.g. [ "yamlFrontMatter" ]
- */
-function locate (methods) {
-  var found = []
-  var i = -1
-
-  while (++i < methods.length) {
-    if (methods[i] && methods[i].match(/FrontMatter$/)) {
-      found.push(methods[i])
-    }
-  }
-
-  return found
-}
-
-/**
- * Formats options passed. If options is a function then assume its a yaml
- * parser. Any keys other than the known options, `name`, `throws` are
- * assumed to be a parser type and put in `parsers`
- *
- * options - Object of options, or a function to parse frontmatter
- * options.throws - Boolean whether to throw when there's an error or not
- * options.name - The name to store parsed frontmatter as in
- */
-function settings (options) {
-  var parsers
-  var throws
-  var name
-
-  if (options && typeof options === 'function') {
-    options = { yaml: options }
-  }
-
-  if (options && typeof options === 'object' && !Array.isArray(options)) {
-    throws = options.throws || false
-    name = options.name || null
-    delete options.throws
-    delete options.name
-  }
-
-  parsers = options
-  options = {
-    parsers: parsers,
-    throws: throws,
-    name: name
-  }
-
-  return options
-}
-
-function isParser (parser) {
-  return Boolean(parser && parser.prototype && parser.prototype.blockMethods)
 }
